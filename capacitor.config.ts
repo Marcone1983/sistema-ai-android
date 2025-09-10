14481944b421e024b763ee940a97b4d0cb6b5703
Sistema AI <sistema@ai.com>
2025-09-10 17:43:45 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sistemaai.android',
+  appName: 'sistema-ai-android',
+  webDir: 'www'
+};
+
+export default config;
