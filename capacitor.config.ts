4bfee57802d895c0bd78c0fcb8707d0bc1f4d342
Marcone1983 <whitecasteddu@gmail.com>
2025-09-11 16:06:05 +0200

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,1 @@
-import type { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'com.sistemaai.android',
-  appName: 'sistema-ai-android',
-  webDir: 'www'
-};
-
-export default config;
